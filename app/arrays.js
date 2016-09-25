e617a6ad1a3257e962fd3f91b9bc8fab441ec69e
Amy McCarthy <amy.mccarthy@ostmodern.co.uk>
2016-09-25 19:48:32 +0100

    inital commit

--- app/arrays.js
+++ app/arrays.js
@@ -2,15 +2,31 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    for(var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
 
+    return -1;
   },
 
   sum: function(arr) {
-
+    var total = 0;
+    for (var i = 0; i < arr.length; i++) {
+      total = total + arr[i];
+    }
+    return total;
   },
 
   remove: function(arr, item) {
-
+    var results = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== item) {
+        results.push(arr[i]);
+      }
+    }
+    return results;
   },
 
   removeWithoutCopy: function(arr, item) {
